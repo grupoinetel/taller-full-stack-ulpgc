6ca235d8f8dc8119b58be5510c81a97d3f82bc1c
Nahima Ortega <nahimaortega@gmail.com>
2026-02-27 21:37:26 +0000

    Se añade la categoría a DetalleTarea

--- angular/src/app/tareas/model/DetalleTarea.ts
+++ angular/src/app/tareas/model/DetalleTarea.ts
@@ -1,12 +1,14 @@
 import { Usuario } from '../../usuarios/model/Usuario';
 import { Comentario } from '../../comentarios/model/Comentario';
 import { PreviewTarea } from './PreviewTarea';
+import {CategoriaTarea} from './CategoriaTarea';
 
 export interface DetalleTarea extends PreviewTarea {
   descripcion: string;
   fechaCreacion: string;
   porcentajeRealizado: number;
   tiempoEstimado: number;
+  categoria: CategoriaTarea;
   autor: Usuario;
   asignados: Usuario[];
   comentarios: Comentario[];
